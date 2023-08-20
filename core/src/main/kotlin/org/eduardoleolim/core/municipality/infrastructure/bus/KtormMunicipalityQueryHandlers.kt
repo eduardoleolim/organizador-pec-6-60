655bd7729ff00c945859f4ae963973220498a2be
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2023-08-20 16:14:40 -0600

    feat(municipality): load query handler in bus

---
+++ core/src/main/kotlin/org/eduardoleolim/core/municipality/infrastructure/bus/KtormMunicipalityQueryHandlers.kt
@@ -0,0 +1,31 @@
+package org.eduardoleolim.core.municipality.infrastructure.bus
+
+import org.eduardoleolim.core.federalEntity.application.search.FederalEntitySearcher
+import org.eduardoleolim.core.federalEntity.infrastructure.persistence.KtormFederalEntityRepository
+import org.eduardoleolim.core.municipality.application.search.MunicipalitySearcher
+import org.eduardoleolim.core.municipality.application.searchByTerm.SearchMunicipalitiesByTermQuery
+import org.eduardoleolim.core.municipality.application.searchByTerm.SearchMunicipalitiesByTermQueryHandler
+import org.eduardoleolim.core.municipality.infrastructure.persistence.KtormMunicipalityRepository
+import org.eduardoleolim.shared.domain.bus.query.Query
+import org.eduardoleolim.shared.domain.bus.query.QueryHandler
+import org.eduardoleolim.shared.domain.bus.query.Response
+import org.ktorm.database.Database
+import kotlin.reflect.KClass
+
+class KtormMunicipalityQueryHandlers(database: Database) :
+    HashMap<KClass<out Query>, QueryHandler<out Query, out Response>>() {
+    private val municipalityRepository: KtormMunicipalityRepository
+    private val federalEntityRepository: KtormFederalEntityRepository
+    private val municipalitySearcher: MunicipalitySearcher
+    private val federalEntitySearcher: FederalEntitySearcher
+
+    init {
+        municipalityRepository = KtormMunicipalityRepository(database)
+        federalEntityRepository = KtormFederalEntityRepository(database)
+        municipalitySearcher = MunicipalitySearcher(municipalityRepository)
+        federalEntitySearcher = FederalEntitySearcher(federalEntityRepository)
+
+        this[SearchMunicipalitiesByTermQuery::class] =
+            SearchMunicipalitiesByTermQueryHandler(municipalitySearcher, federalEntitySearcher)
+    }
+}
--- core/src/main/kotlin/org/eduardoleolim/core/shared/infrastructure/bus/KtormQueryBus.kt
+++ core/src/main/kotlin/org/eduardoleolim/core/shared/infrastructure/bus/KtormQueryBus.kt
@@ -1,6 +1,7 @@
 package org.eduardoleolim.core.shared.infrastructure.bus
 
 import org.eduardoleolim.core.federalEntity.infrastructure.bus.KtormFederalEntityQueryHandlers
+import org.eduardoleolim.core.municipality.infrastructure.bus.KtormMunicipalityQueryHandlers
 import org.eduardoleolim.shared.domain.bus.query.*
 import org.ktorm.database.Database
 import kotlin.reflect.KClass
@@ -12,6 +13,7 @@ class KtormQueryBus(private val database: Database) : QueryBus {
         queryHandlers = HashMap()
         queryHandlers.apply {
             putAll(KtormFederalEntityQueryHandlers(database))
+            putAll(KtormMunicipalityQueryHandlers(database))
         }
     }
 
