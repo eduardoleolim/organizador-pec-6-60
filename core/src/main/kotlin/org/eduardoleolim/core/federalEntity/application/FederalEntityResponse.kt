e02fefa85484459f96c76bdad3340cd383d31266
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2023-09-02 13:18:51 -0600

    refactor: add creation and modification dates to response

--- core/src/main/kotlin/org/eduardoleolim/core/federalEntity/application/FederalEntityResponse.kt
+++ core/src/main/kotlin/org/eduardoleolim/core/federalEntity/application/FederalEntityResponse.kt
@@ -2,15 +2,24 @@ package org.eduardoleolim.core.federalEntity.application
 
 import org.eduardoleolim.core.federalEntity.domain.FederalEntity
 import org.eduardoleolim.shared.domain.bus.query.Response
+import java.util.*
 
-class FederalEntityResponse(val id: String, val keyCode: String, val name: String) : Response {
-    override fun toString(): String {
-        return "FederalEntityResponse(id='$id', keyCode='$keyCode', name='$name')"
-    }
-
+class FederalEntityResponse(
+    val id: String,
+    val keyCode: String,
+    val name: String,
+    val createdAt: Date,
+    val updatedAt: Date?
+) : Response {
     companion object {
         fun fromAggregate(federalEntity: FederalEntity): FederalEntityResponse {
-            return FederalEntityResponse(federalEntity.id().toString(), federalEntity.keyCode(), federalEntity.name())
+            return FederalEntityResponse(
+                federalEntity.id().toString(),
+                federalEntity.keyCode(),
+                federalEntity.name(),
+                federalEntity.createdAt(),
+                federalEntity.updatedAt()
+            )
         }
     }
 }
--- core/src/main/kotlin/org/eduardoleolim/core/municipality/application/MunicipalityResponse.kt
+++ core/src/main/kotlin/org/eduardoleolim/core/municipality/application/MunicipalityResponse.kt
@@ -4,19 +4,25 @@ import org.eduardoleolim.core.federalEntity.application.FederalEntityResponse
 import org.eduardoleolim.core.federalEntity.domain.FederalEntity
 import org.eduardoleolim.core.municipality.domain.Municipality
 import org.eduardoleolim.shared.domain.bus.query.Response
+import java.util.*
+
 
 class MunicipalityResponse(
     val id: String,
     val name: String,
     val keyCode: String,
-    val federalEntity: FederalEntityResponse
+    val federalEntity: FederalEntityResponse,
+    val createdAt: Date,
+    val updatedAt: Date?
 ) : Response {
     companion object {
         fun fromAggregate(municipality: Municipality, federalEntity: FederalEntity) = MunicipalityResponse(
             municipality.id().toString(),
             municipality.name(),
             municipality.keyCode(),
-            FederalEntityResponse.fromAggregate(federalEntity)
+            FederalEntityResponse.fromAggregate(federalEntity),
+            municipality.createdAt(),
+            municipality.updatedAt()
         )
     }
 }
