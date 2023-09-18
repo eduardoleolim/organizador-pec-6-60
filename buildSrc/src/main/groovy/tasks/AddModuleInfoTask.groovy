9cd11209e6226cb71e8a0b2d87cb5f9b40428dc7
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2023-09-17 19:38:54 -0600

    build: add module-info.class to jar with gradle task
    It is an alternative to using the module-info.java file, because of compileJava task throws a ClassCastException when that file exists in the project since kotlin 1.7.0.
    The module-info.class file is necessary to use the jlink plugin.

---
+++ buildSrc/src/main/groovy/tasks/AddModuleInfoTask.groovy
@@ -0,0 +1,135 @@
+package tasks
+
+import org.apache.tools.ant.taskdefs.condition.Os
+import org.gradle.api.DefaultTask
+import org.gradle.api.GradleException
+import org.gradle.api.file.FileCollection
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.TaskAction
+
+abstract class AddModuleInfoTask extends DefaultTask {
+    @Input
+    abstract Property<String> getModuleName()
+
+    @Input
+    abstract Property<String> getMultiRelease()
+
+    @Input
+    abstract Property<String> getJarPath()
+
+    @Input
+    abstract Property<FileCollection> getClasspath()
+
+    AddModuleInfoTask() {
+        group = 'custom'
+        description = 'Add module-info.java to jar file'
+
+        moduleName.convention('org.module.name')
+        jarPath.convention('build/libs/app.jar')
+        classpath.convention(project.files(project.file(".")))
+        multiRelease.convention(null)
+    }
+
+    @TaskAction
+    def addModuleInfo() {
+        def jarFile = new File(jarPath.get())
+        def modulepath = classpath.get().join(File.pathSeparator)
+
+        def moduleInfoFile = createModuleInfo(moduleName.get(), jarFile, modulepath)
+        def moduleClassFile = compileModuleInfo(moduleName.get(), moduleInfoFile, jarFile, modulepath)
+        addModuleInfoToJar(moduleClassFile, jarFile)
+    }
+
+    def createModuleInfo(String moduleName, File jarFile, String modulepath) {
+        def directory = jarFile.parentFile
+        def multiRelease = this.multiRelease.get()
+
+        def listCommand = new ArrayList<String>(['jdeps', '--module-path', modulepath, '--generate-module-info', directory.absolutePath, jarPath.get()].toList())
+        if (multiRelease != null) {
+            listCommand.add(1, '--multi-release')
+            listCommand.add(2, multiRelease)
+        }
+
+        executeCommandLine(listCommand)
+
+        def moduleInfoPath = "${directory.absolutePath}/module-info.java"
+        if (multiRelease != null) {
+            def jarFileName = jarFile.name.replace(".jar", "")
+            moduleInfoPath = "${directory.absolutePath}/${jarFileName}/versions/${this.multiRelease.get()}/module-info.java"
+        }
+
+        def moduleInfoFile = new File(moduleInfoPath)
+
+        if (!moduleInfoFile.exists()) {
+            throw new GradleException("Module-info.java not found")
+        }
+
+        def content = moduleInfoFile.text
+        content = content.replaceFirst("module\\s+[^\\s]+\\s+\\{", "module $moduleName {")
+        moduleInfoFile.write content
+
+        return moduleInfoFile
+    }
+
+    def compileModuleInfo(String moduleName, File moduleInfo, File jarFile, String modulepath) {
+        def directory = jarFile.parentFile
+
+        def listCommand = new ArrayList<String>(['javac', '-d', directory.absolutePath, '--module-path', modulepath, '--patch-module', moduleName + "=" + jarPath.get(), moduleInfo.absolutePath].toList())
+
+        executeCommandLine(listCommand)
+
+        moduleInfo.delete()
+
+        def moduleInfoClass = project.file("${directory.absolutePath}/module-info.class")
+
+        if (!moduleInfoClass.exists()) {
+            throw new GradleException("Module-info.class not found")
+        }
+
+        return moduleInfoClass
+    }
+
+    def addModuleInfoToJar(File moduleInfoClass, File jar) {
+        def directory = jar.parentFile
+
+        def listCommand = new ArrayList<String>(['jar', '-uf', jar.absolutePath, "-C", directory.absolutePath, moduleInfoClass.name].toList())
+
+        executeCommandLine(listCommand)
+
+        moduleInfoClass.delete()
+    }
+
+    def executeCommandLine(List<String> command) {
+        if (command == null || command.isEmpty()) {
+            throw new GradleException("Command is null or empty")
+        }
+
+        if (Os.isFamily(Os.FAMILY_WINDOWS)) {
+            if (command.get(0) != 'cmd') {
+                command.add(0, 'cmd')
+                command.add(1, '/c')
+            }
+        }
+
+        def processBuilder = new ProcessBuilder(command)
+        def process = processBuilder.start()
+        def exitCode = process.waitFor()
+
+        def output = new StringBuilder()
+        def error = new StringBuilder()
+
+        process.inputStream.eachLine { output.append(it) }
+        process.errorStream.eachLine { error.append(it) }
+
+        if (exitCode != 0) {
+            def message = "Error executing command: ${command} \n ${error}"
+
+            if (output != null && !output.isEmpty()) {
+                message = output.toString()
+            }
+
+            throw new GradleException(message)
+        }
+    }
+}
