8f00e7d7195c017ca4b20c9295466cdb50a0884a
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2023-10-13 21:18:50 -0600

    build(core): support java module

---
+++ core/src/main/java/module-info.java
@@ -0,0 +1,31 @@
+module org.eduardoleolim.organizadorpec660.core {
+    requires kotlin.stdlib;
+    requires ktorm.core;
+    requires ktorm.support.sqlite;
+    requires org.eduardoleolim.organizadorpec660.shared;
+    requires org.xerial.sqlitejdbc;
+
+    exports org.eduardoleolim.organizadorpec660.core.federalEntity.application;
+    exports org.eduardoleolim.organizadorpec660.core.federalEntity.application.create;
+    exports org.eduardoleolim.organizadorpec660.core.federalEntity.application.delete;
+    exports org.eduardoleolim.organizadorpec660.core.federalEntity.application.search;
+    exports org.eduardoleolim.organizadorpec660.core.federalEntity.application.searchById;
+    exports org.eduardoleolim.organizadorpec660.core.federalEntity.application.searchByTerm;
+    exports org.eduardoleolim.organizadorpec660.core.federalEntity.application.update;
+    exports org.eduardoleolim.organizadorpec660.core.instrumentType.application;
+    exports org.eduardoleolim.organizadorpec660.core.instrumentType.application.create;
+    exports org.eduardoleolim.organizadorpec660.core.instrumentType.application.delete;
+    exports org.eduardoleolim.organizadorpec660.core.instrumentType.application.search;
+    exports org.eduardoleolim.organizadorpec660.core.instrumentType.application.searchById;
+    exports org.eduardoleolim.organizadorpec660.core.instrumentType.application.searchByTerm;
+    exports org.eduardoleolim.organizadorpec660.core.instrumentType.application.update;
+    exports org.eduardoleolim.organizadorpec660.core.municipality.application;
+    exports org.eduardoleolim.organizadorpec660.core.municipality.application.create;
+    exports org.eduardoleolim.organizadorpec660.core.municipality.application.delete;
+    exports org.eduardoleolim.organizadorpec660.core.municipality.application.search;
+    exports org.eduardoleolim.organizadorpec660.core.municipality.application.searchById;
+    exports org.eduardoleolim.organizadorpec660.core.municipality.application.searchByTerm;
+    exports org.eduardoleolim.organizadorpec660.core.municipality.application.update;
+    exports org.eduardoleolim.organizadorpec660.core.shared.infrastructure.bus;
+    exports org.eduardoleolim.organizadorpec660.core.shared.infrastructure.models;
+}
