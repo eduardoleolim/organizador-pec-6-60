3c3636b64df97819b4990275e5bbc5d686553492
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2023-11-11 18:41:54 -0600

    feat(statistic-type): create record with command

---
+++ core/src/main/kotlin/org/eduardoleolim/organizadorpec660/core/statisticType/application/create/CreateStatisticTypeCommand.kt
@@ -0,0 +1,21 @@
+package org.eduardoleolim.organizadorpec660.core.statisticType.application.create
+
+import org.eduardoleolim.organizadorpec660.shared.domain.bus.command.Command
+
+class CreateStatisticTypeCommand(keyCode: String, name: String, instrumentTypeIds: List<String>) : Command {
+    private val keyCode: String = keyCode.trim().uppercase()
+    private val name: String = name.trim().uppercase()
+    private val instrumentTypeIds: List<String> = instrumentTypeIds.distinct().map { it.trim() }
+
+    fun keyCode(): String {
+        return keyCode
+    }
+
+    fun name(): String {
+        return name
+    }
+
+    fun instrumentTypeIds(): List<String> {
+        return instrumentTypeIds
+    }
+}
