e0b7d25c5dda4292489ac6aeecc44583e68cac75
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2023-11-15 18:55:36 -0600

    feat(titlebar): add titlebar to window

--- app/src/main/kotlin/org/eduardoleolim/organizadorpec660/app/App.kt
+++ app/src/main/kotlin/org/eduardoleolim/organizadorpec660/app/App.kt
@@ -1,9 +1,12 @@
 package org.eduardoleolim.organizadorpec660.app
 
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Column
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.window.application
 import org.eduardoleolim.organizadorpec660.app.utils.ArgsUtils
+import org.eduardoleolim.organizadorpec660.app.views.LightColors
 import org.eduardoleolim.organizadorpec660.app.views.MainWindow
+import org.eduardoleolim.organizadorpec660.app.views.TitleBar
 import org.eduardoleolim.organizadorpec660.core.shared.infrastructure.bus.KtormCommandBus
 import org.eduardoleolim.organizadorpec660.core.shared.infrastructure.bus.KtormQueryBus
 import org.eduardoleolim.organizadorpec660.core.shared.infrastructure.models.SqliteKtormDatabase
@@ -14,21 +17,27 @@ class App(private val commandBus: CommandBus, private val queryBus: QueryBus) {
     fun start() = application {
         MainWindow(
             title = "Organizador PEC-6-60",
+            icon = painterResource("assets/icon.ico"),
             onCloseRequest = ::exitApplication,
+            colorScheme = LightColors,
             minHeight = 600,
             minWidth = 800,
         ) {
-            Row {
-
+            Column {
+                TitleBar(window, ::exitApplication)
             }
         }
     }
 }
 
 fun main(args: Array<String>) {
-    val databasePath = ArgsUtils.getDatabasePath(args) ?: throw Exception("Database path not found")
-    val commandBus = KtormCommandBus(SqliteKtormDatabase.connect(databasePath))
-    val queryBus = KtormQueryBus(SqliteKtormDatabase.connect(databasePath, true))
+    try {
+        val databasePath = ArgsUtils.getDatabasePath(args) ?: throw Exception("Database path not found")
+        val commandBus = KtormCommandBus(SqliteKtormDatabase.connect(databasePath))
+        val queryBus = KtormQueryBus(SqliteKtormDatabase.connect(databasePath, true))
 
-    App(commandBus, queryBus).start()
+        App(commandBus, queryBus).start()
+    } catch (e: Exception) {
+        println(e.message)
+    }
 }
---
+++ app/src/main/kotlin/org/eduardoleolim/organizadorpec660/app/views/TitleBar.kt
@@ -0,0 +1,146 @@
+package org.eduardoleolim.organizadorpec660.app.views
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.gestures.onDrag
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.window.WindowDraggableArea
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Maximize
+import androidx.compose.material.icons.filled.Minimize
+import androidx.compose.material.icons.filled.Restore
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.awt.ComposeWindow
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.input.pointer.PointerEventType
+import androidx.compose.ui.input.pointer.onPointerEvent
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.WindowPlacement
+import androidx.compose.ui.window.WindowScope
+
+@OptIn(ExperimentalFoundationApi::class, ExperimentalComposeUiApi::class)
+@Composable
+fun WindowScope.TitleBar(
+    composeWindow: ComposeWindow,
+    onCloseRequest: () -> Unit
+) {
+    var placement by remember { mutableStateOf(composeWindow.placement) }
+
+    if (placement == WindowPlacement.Fullscreen)
+        return
+
+    val onMaximizeRestoreRequest = {
+        placement = if (placement == WindowPlacement.Maximized) {
+            WindowPlacement.Floating
+        } else {
+            WindowPlacement.Maximized
+        }
+        composeWindow.placement = placement
+    }
+
+    val onMinimizeRequest = {
+        composeWindow.isMinimized = true
+    }
+
+    WindowDraggableArea(
+        modifier = Modifier.fillMaxWidth()
+            .height(40.dp)
+            .background(MaterialTheme.colorScheme.primaryContainer)
+            .pointerInput(Unit) {
+                detectTapGestures(
+                    onDoubleTap = { onMaximizeRestoreRequest() }
+                )
+            }
+            .onDrag {
+                if (placement == WindowPlacement.Maximized) {
+                    placement = WindowPlacement.Floating
+                    composeWindow.placement = placement
+                }
+            }
+    ) {
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = composeWindow.title,
+                modifier = Modifier.padding(start = 10.dp),
+                color = MaterialTheme.colorScheme.onPrimaryContainer
+            )
+
+            Row {
+                var isCloseButtonHovered by remember { mutableStateOf(false) }
+                val buttonModifier = Modifier.size(50.dp, 40.dp).padding(0.dp)
+                val iconModifier = Modifier.size(20.dp)
+                val maximizedIcon = if (placement == WindowPlacement.Maximized) {
+                    Icons.Default.Restore
+                } else {
+                    Icons.Default.Maximize
+                }
+                val buttonColors = IconButtonDefaults.filledIconButtonColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+                )
+                val closeButtonColors = if (isCloseButtonHovered) {
+                    IconButtonDefaults.filledIconButtonColors(
+                        containerColor = MaterialTheme.colorScheme.error,
+                        contentColor = MaterialTheme.colorScheme.onError
+                    )
+                } else {
+                    buttonColors
+                }
+
+                FilledIconButton(
+                    onClick = onMinimizeRequest,
+                    modifier = buttonModifier,
+                    shape = RectangleShape,
+                    colors = buttonColors
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Minimize,
+                        modifier = iconModifier,
+                        contentDescription = "Minimize"
+                    )
+                }
+
+                FilledIconButton(
+                    onClick = onMaximizeRestoreRequest,
+                    modifier = buttonModifier,
+                    shape = RectangleShape,
+                    colors = buttonColors
+                ) {
+                    Icon(
+                        imageVector = maximizedIcon,
+                        modifier = iconModifier,
+                        contentDescription = "Maximize/Restore"
+                    )
+                }
+
+                FilledIconButton(
+                    onClick = onCloseRequest,
+                    modifier = buttonModifier.onPointerEvent(PointerEventType.Enter) {
+                        isCloseButtonHovered = true
+                    }.onPointerEvent(PointerEventType.Exit) {
+                        isCloseButtonHovered = false
+                    },
+                    shape = RectangleShape,
+                    colors = closeButtonColors
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Close,
+                        modifier = iconModifier,
+                        contentDescription = "Close"
+                    )
+                }
+            }
+        }
+    }
+}
