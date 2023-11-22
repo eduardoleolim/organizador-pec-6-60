a45eb6f57e8b9b2e1f9e04a72c8fd657403daf19
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2023-11-21 22:00:30 -0600

    feat(auth): implement auth repository with ktorm

---
+++ core/src/main/kotlin/org/eduardoleolim/organizadorpec660/core/auth/infrastructure/persistence/KtormAuthRepository.kt
@@ -0,0 +1,20 @@
+package org.eduardoleolim.organizadorpec660.core.auth.infrastructure.persistence
+
+import org.eduardoleolim.organizadorpec660.core.auth.domain.Auth
+import org.eduardoleolim.organizadorpec660.core.auth.domain.AuthRepository
+import org.eduardoleolim.organizadorpec660.core.shared.infrastructure.models.Credentials
+import org.ktorm.database.Database
+import org.ktorm.dsl.*
+
+class KtormAuthRepository(private val database: Database) : AuthRepository {
+    private val credentials = Credentials("c")
+
+    override fun search(emailOrUsername: String): Auth? {
+        return database
+            .from(credentials)
+            .select()
+            .where { (credentials.email eq emailOrUsername) or (credentials.username eq emailOrUsername) }
+            .map { Auth.from(emailOrUsername, it[credentials.password]!!) }
+            .firstOrNull()
+    }
+}
