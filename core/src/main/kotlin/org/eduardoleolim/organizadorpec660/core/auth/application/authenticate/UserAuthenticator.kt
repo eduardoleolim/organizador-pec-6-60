d5a85bfbf9e89cb772ad2dbb840d055f0a4c87ef
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2023-11-22 23:18:52 -0600

    refactor(auth): return a boolean result in authenticator

--- core/src/main/kotlin/org/eduardoleolim/organizadorpec660/core/auth/application/authenticate/UserAuthenticator.kt
+++ core/src/main/kotlin/org/eduardoleolim/organizadorpec660/core/auth/application/authenticate/UserAuthenticator.kt
@@ -1,13 +1,11 @@
 package org.eduardoleolim.organizadorpec660.core.auth.application.authenticate
 
 import org.eduardoleolim.organizadorpec660.core.auth.domain.AuthRepository
-import org.eduardoleolim.organizadorpec660.core.auth.domain.InvalidAuthCredentialsError
 
 class UserAuthenticator(private val authRepository: AuthRepository) {
-    fun authenticate(emailOrUsername: String, password: String) {
+    fun authenticate(emailOrUsername: String, password: String): Boolean {
         authRepository.search(emailOrUsername)?.let {
-            if (it.password() != password)
-                throw InvalidAuthCredentialsError()
-        } ?: throw InvalidAuthCredentialsError()
+            return it.password() == password
+        } ?: return false
     }
 }
