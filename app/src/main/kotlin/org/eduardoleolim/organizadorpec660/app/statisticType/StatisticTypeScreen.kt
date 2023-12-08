67c92b03b128abdc2fe6233bcd4b32ab5040798f
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2023-12-07 20:39:30 -0600

    feat(statistic-type): implement base view

---
+++ app/src/main/kotlin/org/eduardoleolim/organizadorpec660/app/statisticType/StatisticTypeScreen.kt
@@ -0,0 +1,20 @@
+package org.eduardoleolim.organizadorpec660.app.statisticType
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import cafe.adriel.voyager.core.screen.Screen
+import org.eduardoleolim.organizadorpec660.shared.domain.bus.command.CommandBus
+import org.eduardoleolim.organizadorpec660.shared.domain.bus.query.QueryBus
+
+class StatisticTypeScreen(private val queryBus: QueryBus, private val commandBus: CommandBus) : Screen {
+    @Composable
+    override fun Content() {
+        Box {
+            Column {
+                Text("Tipos de estadística")
+            }
+        }
+    }
+}
