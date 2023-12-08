2a35f0f71e8f4cdae7c7b542cb5963c50f5b4ccd
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2023-12-07 20:40:21 -0600

    feat(router): add catalog views to router

--- app/src/main/kotlin/org/eduardoleolim/organizadorpec660/app/main/router/Router.kt
+++ app/src/main/kotlin/org/eduardoleolim/organizadorpec660/app/main/router/Router.kt
@@ -5,7 +5,11 @@ import androidx.compose.ui.awt.ComposeWindow
 import cafe.adriel.voyager.core.registry.ScreenRegistry
 import cafe.adriel.voyager.navigator.Navigator
 import org.eduardoleolim.organizadorpec660.app.auth.AuthScreen
+import org.eduardoleolim.organizadorpec660.app.federalEntity.FederalEntityScreen
 import org.eduardoleolim.organizadorpec660.app.home.HomeScreen
+import org.eduardoleolim.organizadorpec660.app.instrumentType.InstrumentTypeScreen
+import org.eduardoleolim.organizadorpec660.app.municipality.MunicipalityScreen
+import org.eduardoleolim.organizadorpec660.app.statisticType.StatisticTypeScreen
 import org.eduardoleolim.organizadorpec660.shared.domain.bus.command.CommandBus
 import org.eduardoleolim.organizadorpec660.shared.domain.bus.query.QueryBus
 
@@ -16,7 +20,23 @@ fun Router(window: ComposeWindow, commandBus: CommandBus, queryBus: QueryBus) {
             AuthScreen(window, queryBus)
         }
         register<MainProvider.HomeScreen> {
-            HomeScreen(window, it.user, commandBus, queryBus)
+            HomeScreen(window, it.user)
+        }
+
+        register<HomeProvider.FederalEntityScreen> {
+            FederalEntityScreen(queryBus, commandBus)
+        }
+
+        register<HomeProvider.MunicipalityScreen> {
+            MunicipalityScreen(queryBus, commandBus)
+        }
+
+        register<HomeProvider.StatisticTypeScreen> {
+            StatisticTypeScreen(queryBus, commandBus)
+        }
+
+        register<HomeProvider.InstrumentTypeScreen> {
+            InstrumentTypeScreen(queryBus, commandBus)
         }
     }
 
