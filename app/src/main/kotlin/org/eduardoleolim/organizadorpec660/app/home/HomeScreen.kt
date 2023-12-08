4d4075a27f8ce31c2d9e04719691df1cec1c422f
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2023-12-07 20:42:31 -0600

    feat(home): add menu and navigation
    Add menu component and navigation between catalogs.

--- app/src/main/kotlin/org/eduardoleolim/organizadorpec660/app/home/HomeScreen.kt
+++ app/src/main/kotlin/org/eduardoleolim/organizadorpec660/app/home/HomeScreen.kt
@@ -1,31 +1,116 @@
 package org.eduardoleolim.organizadorpec660.app.home
 
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ListAlt
+import androidx.compose.material.icons.filled.Logout
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.outlined.ListAlt
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.awt.ComposeWindow
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.model.rememberScreenModel
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
 import org.eduardoleolim.organizadorpec660.core.auth.application.AuthUserResponse
-import org.eduardoleolim.organizadorpec660.shared.domain.bus.command.CommandBus
-import org.eduardoleolim.organizadorpec660.shared.domain.bus.query.QueryBus
 
 class HomeScreen(
     private val window: ComposeWindow,
-    private val user: AuthUserResponse,
-    private val commandBus: CommandBus,
-    private val queryBus: QueryBus
+    private val user: AuthUserResponse
 ) : Screen {
     @Composable
     override fun Content() {
         window.isResizable = true
         val navigator = LocalNavigator.currentOrThrow
-        val screenModel = rememberScreenModel { HomeScreenModel(navigator, commandBus, queryBus) }
+        val screenModel = rememberScreenModel { HomeScreenModel(navigator) }
+        var selectedTab by remember { mutableStateOf(HomeScreenTab.ENTIDADES_FEDERATIVAS) }
 
-        Button(onClick = screenModel::logout) {
-            Text("Click me")
+        Row {
+            NavigationRail(
+                modifier = Modifier.padding(8.dp),
+                header = {
+                    IconButton(
+                        onClick = { },
+                        content = { Icon(Icons.Default.Menu, contentDescription = null) }
+                    )
+                }
+            ) {
+
+                NavigationRailItem(
+                    selected = selectedTab == HomeScreenTab.ENTIDADES_FEDERATIVAS,
+                    onClick = { selectedTab = HomeScreenTab.ENTIDADES_FEDERATIVAS },
+                    modifier = Modifier.height(72.dp),
+                    icon = { Icon(Icons.Default.ListAlt, contentDescription = null) },
+                    label = { Text(text = "Entidades\nfederativas", textAlign = TextAlign.Center) }
+                )
+
+                NavigationRailItem(
+                    selected = selectedTab == HomeScreenTab.MUNICIPIOS,
+                    onClick = { selectedTab = HomeScreenTab.MUNICIPIOS },
+                    icon = {
+                        Icon(
+                            imageVector = if (selectedTab == HomeScreenTab.MUNICIPIOS) Icons.Default.ListAlt else Icons.Outlined.ListAlt,
+                            contentDescription = null
+                        )
+                    },
+                    label = { Text(text = "Municípios", textAlign = TextAlign.Center) }
+                )
+
+                NavigationRailItem(
+                    selected = selectedTab == HomeScreenTab.TIPOS_DE_ESTADISTICA,
+                    onClick = { selectedTab = HomeScreenTab.TIPOS_DE_ESTADISTICA },
+                    modifier = Modifier.height(72.dp),
+                    icon = { Icon(Icons.Default.ListAlt, contentDescription = null) },
+                    label = { Text(text = "Tipos de\nestadísticas", textAlign = TextAlign.Center) }
+                )
+
+                NavigationRailItem(
+                    selected = selectedTab == HomeScreenTab.TIPOS_DE_INSTRUMENTO,
+                    onClick = {
+                        selectedTab = HomeScreenTab.TIPOS_DE_INSTRUMENTO
+                    },
+                    modifier = Modifier.height(72.dp),
+                    icon = { Icon(Icons.Default.ListAlt, contentDescription = null) },
+                    label = { Text(text = "Tipos de\ninstrumentos", textAlign = TextAlign.Center) }
+                )
+
+                NavigationRailItem(
+                    selected = false,
+                    onClick = { screenModel.logout() },
+                    icon = { Icon(Icons.Default.Logout, contentDescription = null) },
+                    label = { Text("Cerrar sesión") }
+                )
+            }
+
+            Divider(
+                color = MaterialTheme.colorScheme.onSurface,
+                modifier = Modifier.fillMaxHeight().width(1.dp)
+            )
+
+            Column(
+                modifier = Modifier.padding(8.dp)
+            ) {
+                when (selectedTab) {
+                    HomeScreenTab.ENTIDADES_FEDERATIVAS -> screenModel.navigateToFederalEntity()
+
+                    HomeScreenTab.MUNICIPIOS -> screenModel.navigateToMunicipality()
+
+                    HomeScreenTab.TIPOS_DE_ESTADISTICA -> screenModel.navigateToStatisticType()
+
+                    HomeScreenTab.TIPOS_DE_INSTRUMENTO -> screenModel.navigateToInstrumentType()
+                }
+            }
         }
     }
+}
+
+enum class HomeScreenTab {
+    ENTIDADES_FEDERATIVAS,
+    MUNICIPIOS,
+    TIPOS_DE_ESTADISTICA,
+    TIPOS_DE_INSTRUMENTO
 }
--- app/src/main/kotlin/org/eduardoleolim/organizadorpec660/app/home/HomeScreenModel.kt
+++ app/src/main/kotlin/org/eduardoleolim/organizadorpec660/app/home/HomeScreenModel.kt
@@ -1,18 +1,38 @@
 package org.eduardoleolim.organizadorpec660.app.home
 
+import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.core.model.ScreenModel
+import cafe.adriel.voyager.core.registry.ScreenRegistry
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.core.stack.popUntil
 import cafe.adriel.voyager.navigator.Navigator
 import org.eduardoleolim.organizadorpec660.app.auth.AuthScreen
-import org.eduardoleolim.organizadorpec660.shared.domain.bus.command.CommandBus
-import org.eduardoleolim.organizadorpec660.shared.domain.bus.query.QueryBus
+import org.eduardoleolim.organizadorpec660.app.main.router.HomeProvider
 
 class HomeScreenModel(
-    private val navigator: Navigator,
-    private val commandBus: CommandBus,
-    private val queryBus: QueryBus
+    private val navigator: Navigator
 ) : ScreenModel {
+
+    @Composable
+    fun navigateToFederalEntity() {
+        Navigator(ScreenRegistry.get(HomeProvider.FederalEntityScreen))
+    }
+
+    @Composable
+    fun navigateToMunicipality() {
+        Navigator(ScreenRegistry.get(HomeProvider.MunicipalityScreen))
+    }
+
+    @Composable
+    fun navigateToStatisticType() {
+        Navigator(ScreenRegistry.get(HomeProvider.StatisticTypeScreen))
+    }
+
+    @Composable
+    fun navigateToInstrumentType() {
+        Navigator(ScreenRegistry.get(HomeProvider.InstrumentTypeScreen))
+    }
+
     fun logout() {
         navigator.popUntil<AuthScreen, Screen>()
     }
