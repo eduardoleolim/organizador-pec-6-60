eb09d0e2582ac5d24ba3f09aba4e30780c6817e2
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2023-12-20 14:32:26 -0600

    refactor(args-parser): simplify search of databasePath

--- app/src/main/kotlin/org/eduardoleolim/organizadorpec660/app/shared/utils/ArgsParser.kt
+++ app/src/main/kotlin/org/eduardoleolim/organizadorpec660/app/shared/utils/ArgsParser.kt
@@ -2,11 +2,6 @@ package org.eduardoleolim.organizadorpec660.app.shared.utils
 
 object ArgsParser {
     fun getDatabasePath(args: Array<String>): String? {
-        args.toList().forEach {
-            if (it.startsWith("--database=")) {
-                return it.replace("--database=", "")
-            }
-        }
-        return null
+        return args.firstOrNull { it.startsWith("--database=") }?.replace("--database=", "")
     }
 }
