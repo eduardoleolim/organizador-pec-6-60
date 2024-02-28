06cd3e3ff2c4507915b4bf8f83c6383161c96929
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2024-02-27 23:29:18 -0600

    refactor: use one application instance for main and config window

--- app/src/main/kotlin/org/eduardoleolim/organizadorPec660/app/Main.kt
+++ app/src/main/kotlin/org/eduardoleolim/organizadorPec660/app/Main.kt
@@ -1,31 +1,16 @@
 package org.eduardoleolim.organizadorPec660.app
 
 import org.eduardoleolim.organizadorPec660.app.main.App
-import org.eduardoleolim.organizadorPec660.app.main.InitializeApp
 import org.eduardoleolim.organizadorPec660.app.shared.utils.AppUtils
-import org.eduardoleolim.organizadorPec660.core.shared.infrastructure.bus.KtormCommandBus
-import org.eduardoleolim.organizadorPec660.core.shared.infrastructure.bus.KtormQueryBus
-import org.eduardoleolim.organizadorPec660.core.shared.infrastructure.models.SqliteKtormDatabase
 
 fun main(args: Array<String>) {
     try {
         System.setProperty("skiko.renderApi", "OPENGL")
         val databasePath = AppUtils.databasePath(args) ?: throw Exception("Database path not found")
-        var sqlitePassword = AppUtils.sqlitePassword()
+        val sqlitePassword = AppUtils.sqlitePassword() ?: throw Exception("Database password not found")
         val sqliteExtensions = AppUtils.sqliteExtensions()
 
-        if (SqliteKtormDatabase.exists(databasePath).not()) {
-            InitializeApp().start { password ->
-                sqlitePassword = password
-                AppUtils.sqlitePassword(password)
-            }
-        }
-
-        val commandBus = KtormCommandBus(SqliteKtormDatabase.connect(databasePath, sqlitePassword!!, sqliteExtensions))
-        val queryBus =
-            KtormQueryBus(SqliteKtormDatabase.connectReadOnly(databasePath, sqlitePassword!!, sqliteExtensions))
-
-        App(commandBus, queryBus).start()
+        App(databasePath, sqlitePassword, sqliteExtensions).start()
     } catch (e: Exception) {
         println(e.localizedMessage)
     }
--- app/src/main/kotlin/org/eduardoleolim/organizadorPec660/app/main/App.kt
+++ app/src/main/kotlin/org/eduardoleolim/organizadorPec660/app/main/App.kt
@@ -1,42 +1,65 @@
 package org.eduardoleolim.organizadorPec660.app.main
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Contrast
 import androidx.compose.material.icons.filled.DarkMode
 import androidx.compose.material.icons.filled.LightMode
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
+import kotlinx.coroutines.launch
 import org.eduardoleolim.organizadorPec660.app.main.customWindow.*
 import org.eduardoleolim.organizadorPec660.app.main.router.Router
 import org.eduardoleolim.organizadorPec660.app.shared.theme.DarkColors
 import org.eduardoleolim.organizadorPec660.app.shared.theme.LightColors
+import org.eduardoleolim.organizadorPec660.app.shared.utils.AppUtils
 import org.eduardoleolim.organizadorPec660.app.shared.utils.isSystemInDarkTheme
 import org.eduardoleolim.organizadorPec660.core.shared.domain.bus.command.CommandBus
-import org.eduardoleolim.organizadorPec660.core.shared.domain.bus.query.QueryBus
+import org.eduardoleolim.organizadorPec660.core.shared.infrastructure.bus.KtormCommandBus
+import org.eduardoleolim.organizadorPec660.core.shared.infrastructure.bus.KtormQueryBus
+import org.eduardoleolim.organizadorPec660.core.shared.infrastructure.models.SqliteKtormDatabase
+import java.awt.Dimension
 
 enum class SystemTheme {
     DARK, LIGHT, DEFAULT
 }
 
-class App(private val commandBus: CommandBus, private val queryBus: QueryBus) {
+class App(
+    private val databasePath: String,
+    private var sqlitePassword: String,
+    private var sqliteExtensions: List<String>
+) {
     fun start() = application {
+        var canInitializeApp by remember { mutableStateOf(SqliteKtormDatabase.exists(databasePath)) }
 
-        MainWindow(
-            onCloseRequest = { exitApplication() }
-        )
+        if (canInitializeApp) {
+            MainWindow(
+                onCloseRequest = { exitApplication() }
+            )
+        } else {
+            ConfigWindow(
+                onCloseRequest = { exitApplication() },
+                onPasswordSet = { password ->
+                    AppUtils.sqlitePassword(password)
+                    sqlitePassword = password
+                    canInitializeApp = true
+                }
+            )
+        }
     }
 
     @Composable
@@ -44,6 +67,12 @@ class App(private val commandBus: CommandBus, private val queryBus: QueryBus) {
         val state = rememberWindowState()
         var selectedTheme by remember { mutableStateOf(SystemTheme.DEFAULT) }
         val isSystemInDarkTheme = isSystemInDarkTheme()
+        val commandBus: CommandBus = remember {
+            KtormCommandBus(SqliteKtormDatabase.connect(databasePath, sqlitePassword, sqliteExtensions))
+        }
+        val queryBus = remember {
+            KtormQueryBus(SqliteKtormDatabase.connectReadOnly(databasePath, sqlitePassword, sqliteExtensions))
+        }
 
         MaterialTheme(
             colorScheme = when (selectedTheme) {
@@ -88,6 +117,91 @@ class App(private val commandBus: CommandBus, private val queryBus: QueryBus) {
                 }
 
                 Router(commandBus, queryBus)
+            }
+        }
+    }
+
+    @Composable
+    private fun ConfigWindow(onCloseRequest: () -> Unit, onPasswordSet: (String) -> Unit) {
+        val state = rememberWindowState()
+        val isSystemInDarkTheme = isSystemInDarkTheme()
+
+        MaterialTheme(
+            colorScheme = if (isSystemInDarkTheme) DarkColors else LightColors
+        ) {
+            val icon = painterResource("assets/icon.png")
+            CustomWindow(
+                state = state,
+                onCloseRequest = onCloseRequest,
+                onRequestToggleMaximize = null,
+                defaultTitle = "Organizador PEC-6-60",
+                defaultIcon = icon
+            ) {
+                LaunchedEffect(Unit) {
+                    window.apply {
+                        isResizable = false
+                        size = Dimension(400, 400)
+                        setLocationRelativeTo(null)
+                    }
+                }
+
+                WindowIcon(icon) {}
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    val coroutineScope = rememberCoroutineScope()
+                    var password by remember { mutableStateOf("") }
+
+                    Text(
+                        text = "¡Bienvenido a Organizador PEC-6-60!",
+                        style = TextStyle(
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                    )
+
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    Text(
+                        text = "Para comenzar, establezca una contraseña para la base de datos",
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+                        style = TextStyle(
+                            fontSize = 16.sp,
+                            color = Color.Gray
+                        )
+                    )
+
+                    Spacer(modifier = Modifier.height(32.dp))
+
+                    OutlinedTextField(
+                        value = password,
+                        onValueChange = { password = it },
+                        label = { Text("Contraseña") }
+                    )
+
+                    Spacer(modifier = Modifier.height(32.dp))
+
+                    Button(
+                        onClick = {
+                            coroutineScope.launch {
+
+                            }
+                            onPasswordSet(password)
+                        },
+                        modifier = Modifier.width(200.dp)
+                    ) {
+                        Text(
+                            text = "Guardar",
+                            fontSize = 16.sp
+                        )
+                    }
+                }
             }
         }
     }
