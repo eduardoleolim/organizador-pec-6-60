7694ab054f8ed027b6be8526e80f124b26be5e2f
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2024-03-17 01:15:22 -0600

    fix(app): set value skiko.renderApi
    Fix EXCEPTION_ACCESS_VIOLATION (0xc0000005)

--- app/src/main/kotlin/org/eduardoleolim/organizadorPec660/app/Main.kt
+++ app/src/main/kotlin/org/eduardoleolim/organizadorPec660/app/Main.kt
@@ -5,6 +5,15 @@ import org.eduardoleolim.organizadorPec660.app.shared.utils.AppUtils
 
 fun main(args: Array<String>) {
     try {
+        val osName = System.getProperty("os.name").lowercase()
+
+        val renderApi = when {
+            osName.contains("win") -> "OPENGL"
+            osName.contains("mac") -> "METAL"
+            else -> "OPENGL"
+        }
+
+        System.setProperty("skiko.renderApi", renderApi)
         val databasePath = AppUtils.databasePath(args) ?: throw Exception("Database path not found")
         val sqlitePassword = AppUtils.sqlitePassword() ?: throw Exception("Database password not found")
         val sqliteExtensions = AppUtils.sqliteExtensions()
