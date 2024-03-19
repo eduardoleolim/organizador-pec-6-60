8b5dbc52484c7b50aecef0cd6cf997c2c1c22dd7
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2024-03-18 19:35:16 -0600

    feat(statistic-type): implement table for searching and model
    Implement table for searching and screen-model for create, update, delete and search operations

--- app/src/main/kotlin/org/eduardoleolim/organizadorPec660/app/statisticType/StatisticTypeScreen.kt
+++ app/src/main/kotlin/org/eduardoleolim/organizadorPec660/app/statisticType/StatisticTypeScreen.kt
@@ -1,26 +1,43 @@
 package org.eduardoleolim.organizadorPec660.app.statisticType
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SmallFloatingActionButton
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import cafe.adriel.voyager.core.model.rememberScreenModel
 import cafe.adriel.voyager.core.screen.Screen
+import com.seanproctor.datatable.paging.rememberPaginatedDataTableState
 import org.eduardoleolim.organizadorPec660.core.shared.domain.bus.command.CommandBus
 import org.eduardoleolim.organizadorPec660.core.shared.domain.bus.query.QueryBus
+import org.eduardoleolim.organizadorPec660.core.statisticType.application.StatisticTypeResponse
 
 class StatisticTypeScreen(private val queryBus: QueryBus, private val commandBus: CommandBus) : Screen {
     @Composable
     override fun Content() {
+        val screenModel = rememberScreenModel { StatisticTypeScreenModel(queryBus, commandBus) }
+        var showDeleteModal by remember { mutableStateOf(false) }
+        var showFormModal by remember { mutableStateOf(false) }
+        var selectedStatisticType by remember { mutableStateOf<StatisticTypeResponse?>(null) }
+        val pageSizes = remember { listOf(10, 25, 50, 100) }
+        val state = rememberPaginatedDataTableState(pageSizes.first())
+        var searchValue by remember { mutableStateOf("") }
+
+        fun resetView() {
+            searchValue = ""
+            state.pageIndex = -1
+            state.pageSize = pageSizes.first()
+            showDeleteModal = false
+            showFormModal = false
+            selectedStatisticType = null
+        }
+
         Column(
             modifier = Modifier.padding(24.dp)
         ) {
@@ -54,7 +71,30 @@ class StatisticTypeScreen(private val queryBus: QueryBus, private val commandBus
                 }
             }
 
-            // Table
+            StatisticTypeTable(
+                modifier = Modifier.fillMaxSize(),
+                value = searchValue,
+                onValueChange = { searchValue = it },
+                pageSizes = pageSizes,
+                state = state,
+                data = screenModel.statisticTypes.value,
+                onSearch = { search, pageIndex, pageSize, orderBy, isAscending ->
+                    val orders = orderBy?.let {
+                        val orderType = if (isAscending) "ASC" else "DESC"
+                        arrayOf(hashMapOf("orderBy" to orderBy, "orderType" to orderType))
+                    }
+
+                    screenModel.searchStatisticTypes(search, orders, pageSize, pageIndex * pageSize)
+                },
+                onDeleteRequest = { federalEntity ->
+                    selectedStatisticType = federalEntity
+                    showDeleteModal = true
+                },
+                onEditRequest = { federalEntity ->
+                    selectedStatisticType = federalEntity
+                    showFormModal = true
+                }
+            )
         }
     }
 }
