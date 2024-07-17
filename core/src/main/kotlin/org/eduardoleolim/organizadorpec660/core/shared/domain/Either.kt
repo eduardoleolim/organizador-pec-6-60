7b9e41f01c561ad1168b12b27194a23144905437
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2024-07-17 05:00:03 -0600

    feat(either): implement async fold

--- core/src/main/kotlin/org/eduardoleolim/organizadorpec660/core/shared/domain/Either.kt
+++ core/src/main/kotlin/org/eduardoleolim/organizadorpec660/core/shared/domain/Either.kt
@@ -2,14 +2,17 @@ package org.eduardoleolim.organizadorpec660.core.shared.domain
 
 sealed class Either<out L, out R> {
     abstract fun <C> fold(ifLeft: (L) -> C, ifRight: (R) -> C): C
+    abstract suspend fun <C> foldAsync(ifLeft: suspend (L) -> C, ifRight: suspend (R) -> C): C
 }
 
 data class Right<out R>(val value: R) : Either<Nothing, R>() {
     override fun <C> fold(ifLeft: (Nothing) -> C, ifRight: (R) -> C): C = ifRight(value)
+    override suspend fun <C> foldAsync(ifLeft: suspend (Nothing) -> C, ifRight: suspend (R) -> C): C = ifRight(value)
 }
 
 data class Left<out L>(val value: L) : Either<L, Nothing>() {
     override fun <C> fold(ifLeft: (L) -> C, ifRight: (Nothing) -> C): C = ifLeft(value)
+    override suspend fun <C> foldAsync(ifLeft: suspend (L) -> C, ifRight: suspend (Nothing) -> C): C = ifLeft(value)
 }
 
 inline fun <L, R> Either<L, R>.onRight(action: (R) -> Unit): Either<L, R> {
