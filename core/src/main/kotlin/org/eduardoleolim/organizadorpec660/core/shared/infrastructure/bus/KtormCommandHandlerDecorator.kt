ef146c7cfcdf9ccc0adc701d71edb9c3c3b9a4a3
Ángel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2024-08-17 19:48:35 -0600

    fix(command-handler): rollback transaction if either is left

--- core/src/main/kotlin/org/eduardoleolim/organizadorpec660/core/shared/infrastructure/bus/KtormCommandHandlerDecorator.kt
+++ core/src/main/kotlin/org/eduardoleolim/organizadorpec660/core/shared/infrastructure/bus/KtormCommandHandlerDecorator.kt
@@ -3,6 +3,7 @@ package org.eduardoleolim.organizadorpec660.core.shared.infrastructure.bus
 import org.eduardoleolim.organizadorpec660.core.shared.domain.Either
 import org.eduardoleolim.organizadorpec660.core.shared.domain.bus.command.Command
 import org.eduardoleolim.organizadorpec660.core.shared.domain.bus.command.CommandHandler
+import org.eduardoleolim.organizadorpec660.core.shared.domain.onLeft
 import org.ktorm.database.Database
 
 class KtormCommandHandlerDecorator<L, R, T : Command<L, R>>(
@@ -10,8 +11,10 @@ class KtormCommandHandlerDecorator<L, R, T : Command<L, R>>(
     private val commandHandler: CommandHandler<L, R, T>
 ) : CommandHandler<L, R, T> {
     override fun handle(command: T): Either<L, R> {
-        return database.useTransaction {
-            commandHandler.handle(command)
+        return database.useTransaction { transaction ->
+            commandHandler.handle(command).onLeft {
+                transaction.rollback()
+            }
         }
     }
 }
