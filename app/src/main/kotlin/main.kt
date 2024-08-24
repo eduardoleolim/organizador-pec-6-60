cd370d7fe2c805e267b79728b5541327cc5d7474
Ángel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2024-08-23 18:25:01 -0600

    refactor(app): add prefix "app" to all properties

--- app/src/main/kotlin/main.kt
+++ app/src/main/kotlin/main.kt
@@ -3,9 +3,9 @@ import org.eduardoleolim.organizadorpec660.app.shared.utils.AppConfig
 
 fun main() {
     try {
-        val databaseDirectory = AppConfig["database.dir"] ?: error("Database path not found")
-        val password = AppConfig["database.password"] ?: error("Database password not found")
-        val extensionsDirectory = AppConfig["database.extensions.dir"] ?: error("Database extension path not found")
+        val databaseDirectory = AppConfig["app.database.dir"] ?: error("Database path not found")
+        val password = AppConfig["app.database.password"] ?: error("Database password not found")
+        val extensionsDirectory = AppConfig["app.database.extensions.dir"] ?: error("Database extension path not found")
         val instrumentsDirectory = AppConfig["app.instruments.dir"] ?: error("Instruments path not found")
         val tempDirectory = AppConfig["app.temp.dir"] ?: run {
             val defaultTempDir = "\${java.io.tmpdir}/organizador-pec-6-60"
