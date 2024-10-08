633bd1edfcbb5e4597f5ce2319b76ce2233ce968
Eduardo Martínez <eduardoleolim@hotmail.com>
2024-10-07 23:26:38 -0600

    chore: add license's header

--- app/src/main/kotlin/Main.kt
+++ app/src/main/kotlin/Main.kt
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2024 Ángel Eduardo Martínez Leo Lim
+ * This file is part of organizador-pec-6-60.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import org.eduardoleolim.organizadorpec660.App
 import org.eduardoleolim.organizadorpec660.shared.utils.AppConfig
 import org.eduardoleolim.organizadorpec660.shared.utils.generateErrorsLog
@@ -12,14 +30,12 @@ fun main() {
             AppConfig["app.database.extensions.dir"] ?: error("Database extensions directory is not defined")
         val instrumentsDir = AppConfig["app.instruments.dir"] ?: error("Instrument files directory is not defined")
         val tempDir = AppConfig["app.temp.dir"] ?: error("Temp directory is not defined")
-
         val absoluteDatabaseDir = dataDir.resolve(databaseDir).normalize().absolutePath
         val absoluteInstrumentsDir = dataDir.resolve(instrumentsDir).normalize().absolutePath
 
         App(absoluteDatabaseDir, databasePassword, databaseExtensionsDir, absoluteInstrumentsDir, tempDir).start()
     } catch (e: Exception) {
         println(e.localizedMessage)
-
         generateErrorsLog("main", e)
     }
 }
--- core/src/main/kotlin/org/eduardoleolim/organizadorpec660/agency/domain/Agency.kt
+++ core/src/main/kotlin/org/eduardoleolim/organizadorpec660/agency/domain/Agency.kt
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2024 Ángel Eduardo Martínez Leo Lim
+ * This file is part of organizador-pec-6-60.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.eduardoleolim.organizadorpec660.agency.domain
 
 import org.eduardoleolim.organizadorpec660.municipality.domain.MunicipalityId
@@ -14,8 +32,8 @@ import java.util.*
  * @property statisticTypeIds The ids of statistic types used to generate the summary reports
  * @property createdAt The date when the agency was registered in the system
  * @property updatedAt The date of last update of agency
- * @see org.eduardoleolim.organizadorpec660.core.municipality.domain.Municipality
- * @see org.eduardoleolim.organizadorpec660.core.statisticType.domain.StatisticType
+ * @see org.eduardoleolim.organizadorpec660.municipality.domain.Municipality
+ * @see org.eduardoleolim.organizadorpec660.statisticType.domain.StatisticType
  * @constructor Create Agency
  */
 class Agency private constructor(
